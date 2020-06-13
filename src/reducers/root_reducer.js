68d99995069adbc1d952f21a4906b1582abbd0fb
unknown <dannythanhphan@gmail.com>
2020-06-13 10:33:29 -0700

    Remove redux

--- src/reducers/root_reducer.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from "redux";
-import rewardReducer from './reward_reducer';
-
-const rootReducer = combineReducers({
-  reward: rewardReducer,
-});
-
-export default rootReducer;
